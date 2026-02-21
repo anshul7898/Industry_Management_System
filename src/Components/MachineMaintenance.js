58d8b5dcc591e0766fff0a12a053336b179be8b3
anshul mishra <anshul7898@yhaoo.com>
2026-02-21 11:10:53 +0530

    Implementing FeatureCommingSoon Component

--- src/Components/MachineMaintenance.js
+++ src/Components/MachineMaintenance.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from './Navbar';
+import FeatureCommingSoon from './FeatureCommingSoon';
 export default function MachineMaintenance() {
   return (
     <div style={{ width: '100%', textAlign: 'center' }}>
       <Navbar />
       <h1>Machine Maintenance</h1>
+      <FeatureCommingSoon />
     </div>
   );
 }
