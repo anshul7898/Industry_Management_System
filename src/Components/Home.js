8c9482ac7bdc380148ee9a93546cf6d3ca6fc209
anshul mishra <anshul7898@yhaoo.com>
2026-02-21 11:17:03 +0530

    Implementing FeatureCommingSoon on Delivery and Home page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,9 +1,11 @@
 import Navbar from './Navbar';
+import FeatureCommingSoon from './FeatureCommingSoon';
 export default function Home() {
   return (
     <div style={{ width: '100%', textAlign: 'center' }}>
       <Navbar />
       <h1>Home Component</h1>
+      <FeatureCommingSoon />
     </div>
   );
 }
